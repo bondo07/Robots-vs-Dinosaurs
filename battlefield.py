297749d97e0329433e39e3f36b5bd3fbae53b22d
Armando Beltran <armando_b@live.com>
2022-09-26 11:02:06 -0500

    Created Battlefield class

---
+++ battlefield.py
@@ -0,0 +1,19 @@
+from robot import Robot
+from dinosaur import Dinosaur
+
+class Battlefield:
+    def __init__(self):
+        self.robot = Robot
+        self.dinosaur = Dinosaur
+
+    def run_game(self):
+        return
+
+    def display_welcome(self):
+        print('n\Welcome to Robots vs Dinosaurs!')
+
+    def battle_phase(self):
+        return
+    
+    def display_winner(self):
+        return
