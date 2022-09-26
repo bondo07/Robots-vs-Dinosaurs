90bacfa4963c8e3e8d8723745d81e5dee06ad295
Armando Beltran <armando_b@live.com>
2022-09-26 10:34:39 -0500

    Created Robot class

---
+++ robot.py
@@ -0,0 +1,9 @@
+from weapon import Weapon
+class Robot:
+    def __init__(self, name):
+        self.name = ""
+        self.health = int
+        self.active_weapon = Weapon
+
+    def attack(self, dinosaur):
+        return
