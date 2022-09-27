2726615bd96fc03616ce563616ada7236001483b
Armando Beltran <armando_b@live.com>
2022-09-27 11:41:45 -0500

    Created and got battlephase method loop working

--- battlefield.py
+++ battlefield.py
@@ -19,19 +19,18 @@ class Battlefield:
     def battle_phase(self):
         turn_1 = random.randint(1,2)
         if turn_1 == 1:
-            print(f"Robot {self.robot.name} gets to attack first!\n")
+           print(f"Robot {self.robot.name} gets to attack first!\n")
         elif turn_1 == 2:
             print(f"Dino {self.dinosaur.name} gets to attack first!\n")
-
-        while turn_1 == 1:
-            self.robot.attack(self.dinosaur)
-            turn_1 = 2
-        else:
-            self.dinosaur.attack(self.robot)
-            turn_1 = 1
-        # if turn_1 == 1:
-        #     self.robot.attack(self.dinosaur)
-        # elif turn_1 == 2:
-        #     self.dinosaur.attack(self.robot)
+        while self.dinosaur.health >= 0 and self.robot.health >= 0:
+            if turn_1 == 1:
+                self.robot.attack(self.dinosaur)
+                print()
+                turn_1 = 2
+            elif turn_1 == 2:
+                self.dinosaur.attack(self.robot)
+                print()
+                turn_1 = 1
+        
     # def display_winner(self):
     #     return
